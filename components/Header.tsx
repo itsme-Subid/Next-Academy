90c5918532168e6b38a1628a8c1bd82d6b97504f
SUBID DAS <itsmesubid@gmail.com>
2022-12-30 21:35:57 +0530

    `Infotech Success Point` Init

---
+++ components/Header.tsx
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+
+// every unit will be in rem
+
+const Nav = styled.nav`
+  height: 5rem;
+  width: 100%;
+  background: transparent;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 2rem;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 100;
+  transition: all 0.15s ease;
+  & h1 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    letter-spacing: 0.05rem;
+    color: rgba(var(--light-color), 0.75);
+    cursor: pointer;
+    transition: all 0.15s ease;
+    &:hover {
+      color: rgba(var(--light-color), 1);
+    }
+  }
+  & ul {
+    display: flex;
+    gap: 2rem;
+    list-style: none;
+    & li {
+      font-size: 1rem;
+      font-weight: 500;
+      letter-spacing: 0.05rem;
+      cursor: pointer;
+      color: rgba(var(--primary-color), 0.75);
+      transition: all 0.15s ease;
+      font-weight: 600;
+      &:hover {
+        color: rgba(var(--primary-color), 1);
+      }
+    }
+  }
+`;
+
+const PseudoElement = styled.div`
+  height: 5rem;
+`;
+
+const Header = () => {
+  return (
+    <>
+      <Nav>
+        <h1>Infotech Success Point</h1>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+        </ul>
+      </Nav>
+      <PseudoElement />
+    </>
+  );
+};
+
+export default Header;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,72 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Inter } from "@next/font/google";
+import styled, { createGlobalStyle } from "styled-components";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+
+const GlobalStyle = createGlobalStyle`
+:root {
+  --light-color: 235, 236, 241;
+  --dark-color: 13, 14, 18;
+  --primary-color: 180, 129, 88;
+  --primary-gradient-1: 253, 167, 76;
+  --secondary-color: 31, 111, 235;
+  --secondary-gradient-1: 109, 166, 253;
+  --white-color: 255, 255, 255;
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: var(--font-inter, sans-serif);
+  outline: none;
+}
+body {
+  background: rgba(var(--dark-color), 1);
+  color: rgba(var(--light-color), 1);
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+  transition: all 0.15s ease;
+}
+.container {
+  margin-inline: auto;
+  width: min(90%, 70rem);
+}
+.App {
+  min-height: 100vh;
+  display: grid;
+  place-items: center;
+}
+::-webkit-scrollbar {
+  width: 0.25em;
+}
+::-webkit-scrollbar-track {
+  background: transparent;
+  transition: all 0.15s ease;
+}
+::-webkit-scrollbar-track:hover {
+  background: rgba(var(--dark-color), 0.01);
+}
+::-webkit-scrollbar-thumb {
+  background: rgba(var(--light-color), 0.25);
+  border-radius: 0.5rem;
+}
+a {
+  text-decoration: none;
+  color: inherit;
+  transition: 0.15s;
+}
+`;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={inter.variable}>
+      <GlobalStyle />
+      <Header />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  );
 }
