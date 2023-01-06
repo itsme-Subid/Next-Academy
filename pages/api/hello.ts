63024b4a1bd0c7cc6181bcd8d5d3abe1f3bb7523
SUBID DAS <itsmesubid@gmail.com>
2023-01-06 23:43:05 +0530

    Change `api`

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { readFile } from "fs";
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  readFile("pages/api/hello.ts", "utf8", (err, data) => {
+    if (!!err) {
+      res.status(500).json({ name: err.message });
+    }
+    res.status(200).json({ name: data });
+  });
 }
